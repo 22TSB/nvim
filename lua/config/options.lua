d43b5394b297d46eb1cdc4ccd8080287d6bb0a15
22TSB <166315558+22TSB@users.noreply.github.com>
2026-08-30 23:57:45 +0300

    indent and clean options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,11 @@
 -- OPTIONS
 local set = vim.opt
 
+
 --line nums
 set.relativenumber = true
 set.number = true
+
 
 -- indentation and tabs
 set.tabstop = 4
@@ -11,36 +13,46 @@ set.shiftwidth = 4
 set.autoindent = true
 set.expandtab = true
 
+
 -- search settings
 set.ignorecase = true
 set.smartcase = true
+
 
 -- appearance
 set.termguicolors = true
 set.background = "dark"
 set.signcolumn = "yes"
 
+
 -- cursor line
 set.cursorline = true
+
 
 -- 80th column
 -- set.colorcolumn = "80"
 
+
 -- clipboard
 set.clipboard:append("unnamedplus")
 
+
 -- backspace
 set.backspace = "indent,eol,start"
+
 
 -- split windows
 set.splitbelow = true
 set.splitright = true
 
+
 -- dw/diw/ciw works on full-word
 set.iskeyword:append("-")
 
+
 -- keep cursor at least 8 rows from top/bot
 set.scrolloff = 8
+
 
 -- undo dir settings
 set.swapfile = false
@@ -48,11 +60,14 @@ set.backup = false
 set.undodir = os.getenv("HOME") .. "/.vim/undodir"
 set.undofile = true
 
+
 -- incremental search
 set.incsearch = true
 
+
 -- faster cursor hold
 set.updatetime = 50
+
 
 -- ide like highlight when stopping cursor
 vim.api.nvim_create_autocmd("CursorMoved", {
@@ -79,6 +94,7 @@ vim.api.nvim_create_autocmd("CursorMoved", {
     end,
 })
 
+
 -- ide like highlight when stopping cursor
 vim.api.nvim_create_autocmd("CursorMovedI", {
     group = "LspReferenceHighlight",
@@ -87,6 +103,7 @@ vim.api.nvim_create_autocmd("CursorMovedI", {
         vim.lsp.buf.clear_references()
     end,
 })
+
 
 vim.opt.wrap = true      -- enable soft wrapping
 vim.opt.linebreak = true -- wrap at word boundaries
